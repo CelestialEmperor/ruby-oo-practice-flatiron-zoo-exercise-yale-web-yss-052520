004ac36131cd11fbc975a1908ba86bb8771b5880
Jack Ma <mapeiyao@gmail.com>
2020-06-03 13:37:21 +0000

    Automatically backed up by Learn

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,27 @@
 class Animal
-
+  
+  attr_reader :species, :nickname
+  attr_accessor :weight, :zoo
+  
+  @@all = []
+  
+  def initialize(species, nickname, weight)
+    @species = species
+    @nickname =  nickname
+    @weight = weight
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def zoo
+    Zoo.all.select{|zoo| zoo.animal == self}
+  end
+  
+  def self.find_by_species(species)
+    all.select(&:species)
+  end
+  
 end
--- run.rb
+++ run.rb
@@ -2,9 +2,18 @@ require_relative "lib/Animal.rb"
 require_relative "lib/Zoo.rb"
 require 'pry'
 
-
 #Test your code here
 
+zooa = Zoo.new("Zoo A", "Location A")
+zoob = Zoo.new("Zoo B", "Location B")
+zooc = Zoo.new("Zoo C", "Location C")
+
+animal1 = Animal.new("Dog", "Jack", 100)
+animal2 = Animal.new("Cat", "Jackson", 200)
+animal3 = Animal.new("Rat", "Jake", 10)
+animal1.zoo = zooa
+animal2.zoo = zoob
+animal3.zoo = zooc
 
 binding.pry
 puts "done"
